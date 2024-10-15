48e6cc7ff265d5aeeb176df8ab67533ef8f33128
ClauellaN <clauenikiz@gmail.com>
2024-10-15 16:47:11 -0400

    Updating projects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,248 +1,253 @@
-
 import React from "react";
 import { useState } from "react";
 import styled from "styled-components";
-import {FaEnvelope, FaPhone} from 'react-icons/fa';
-import emailjs from 'emailjs-com'
+import { FaEnvelope, FaPhone } from "react-icons/fa";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: ''
-    });
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-    const [submitted, setSubmitted] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
 
-        // Hide success message when the user starts typing again
-        if (submitted) {
-            setSubmitted(false);
-        }
-    };
+    // Hide success message when the user starts typing again
+    if (submitted) {
+      setSubmitted(false);
+    }
+  };
 
-    //FUNCTION TO HANDLE FORM SUBMISSION USING EMAILJS
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        emailjs.sendForm('service_txfwapk' , 'template_1vym2tc' , e.target, 'w5x3poBS7GxrL9xT2' )
-        .then ((result) => {
-            console.log(result.text)
-            setSubmitted(true);
-            setFormData({
-                name: '',
-                email: '',
-                message: ''
-            });
-            (error) => {
-                console.log(error.text)
-            }
+  //FUNCTION TO HANDLE FORM SUBMISSION USING EMAILJS
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_txfwapk",
+        "template_1vym2tc",
+        e.target,
+        "w5x3poBS7GxrL9xT2"
+      )
+      .then((result) => {
+        console.log(result.text);
+        setSubmitted(true);
+        setFormData({
+          name: "",
+          email: "",
+          message: "",
         });
-        // Hiding the sent message after 5 seconds
-        setTimeout(() => {
-            setSubmitted(false);
-        }, 5000);
-    };
-    return (
-        <ContactWrapper>
-            <Title>CONTACT ME</Title>
-            <ContactInfo>
-                <InfoSection>
-                <SocialLink href="tel:+15147024250" target="_blank">
-                    <FaPhone size={20} />
-                </SocialLink> 
-                    <ContactText>+1(514)702-4250</ContactText>
-                </InfoSection>
-                <InfoSection>
-                  <SocialLink href="https://gmail.com/" target="_blank">
-                    <FaEnvelope size={20} /> 
-                  </SocialLink>
-                    <ContactText>clauenikiz@gmail.com</ContactText>
-                </InfoSection>
-            </ContactInfo>
-            <Form onSubmit={handleSubmit}>
-                <FormField>
-                    <Input
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        placeholder="Your full name"
-                        required
-                    />
-                </FormField>
-                <FormField>
-                    <Input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        placeholder="Your email address"
-                        required
-                    />
-                </FormField>
-                <FormField>
-                    <Textarea
-                        id="message"
-                        name="message"
-                        value={formData.message}
-                        onChange={handleChange}
-                        placeholder="Your message"
-                        required
-                    />
-                </FormField>
-                <ButtonContainer>
-                    <SubmitButton type="submit">SEND</SubmitButton>
-                </ButtonContainer>
-                {submitted && <SentMessage> Your message has been sent!</SentMessage>}
-            </Form>
-        </ContactWrapper>
-    );
+        (error) => {
+          console.log(error.text);
+        };
+      });
+    // Hiding the sent message after 5 seconds
+    setTimeout(() => {
+      setSubmitted(false);
+    }, 5000);
+  };
+  return (
+    <ContactWrapper>
+      <Title>CONTACT ME</Title>
+      <ContactInfo>
+        <InfoSection>
+          <SocialLink href="tel:+15147024250" target="_blank">
+            <FaPhone size={20} />
+          </SocialLink>
+          <ContactText>+1(514)702-4250</ContactText>
+        </InfoSection>
+        <InfoSection>
+          <SocialLink href="https://gmail.com/" target="_blank">
+            <FaEnvelope size={20} />
+          </SocialLink>
+          <ContactText>clauenikiz@gmail.com</ContactText>
+        </InfoSection>
+      </ContactInfo>
+      <Form onSubmit={handleSubmit}>
+        <FormField>
+          <Input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Your full name"
+            required
+          />
+        </FormField>
+        <FormField>
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Your email address"
+            required
+          />
+        </FormField>
+        <FormField>
+          <Textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            placeholder="Your message"
+            required
+          />
+        </FormField>
+        <ButtonContainer>
+          <SubmitButton type="submit">SEND</SubmitButton>
+        </ButtonContainer>
+        {submitted && <SentMessage> Your message has been sent!</SentMessage>}
+      </Form>
+    </ContactWrapper>
+  );
 };
 
 export default Contact;
 
 // styled components
 const ContactWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 40px;
-    color: black;
-    min-height: 100vh;
-    background-color: #1a1a1a;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 40px;
+  color: black;
+  min-height: 100vh;
+  background-color: #1a1a1a;
 `;
 
 const ContactInfo = styled.div`
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    max-width: 800px;
-    margin-bottom: 50px;
-    background-color: #333333;
-    padding: 20px;
-    border-radius: 15px;
-    flex-wrap: wrap;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 800px;
+  margin-bottom: 50px;
+  background-color: #333333;
+  padding: 20px;
+  border-radius: 15px;
+  flex-wrap: wrap;
 
-    @media (max-width: 768px) {
-        flex-direction: column;
-        align-items: center;
-    }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 const Title = styled.h2`
   font-size: 2rem;
   margin-bottom: 100px;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   color: white;
 
-@media (max-width: 768px) {
-font-size: 1.5rem;
+  @media (max-width: 768px) {
+    font-size: 1.5rem;
   }
 `;
 
 const InfoSection = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    flex: 1;
-    color: white;
-    padding: 10px;
-    text-align: center;
-    min-width: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  color: white;
+  padding: 10px;
+  text-align: center;
+  min-width: 150px;
 
-    @media (max-width: 768px) {
-        width: 100%;
-    }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 const ContactText = styled.span`
-    margin-top: 30px;
-    font-size: 1.5rem;
-    color: white;
+  margin-top: 30px;
+  font-size: 1.5rem;
+  color: white;
 `;
 
 const SocialLink = styled.a`
-    color: white;
-    transition: color 0.3s;
+  color: white;
+  transition: color 0.3s;
 
-    &:hover {
-        color: #007bff;
-    }
+  &:hover {
+    color: #007bff;
+  }
 `;
 const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    max-width: 800px;
-    background-color: #333333;
-    padding: 20px;
-    border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 800px;
+  background-color: #333333;
+  padding: 20px;
+  border-radius: 15px;
 `;
 
 const FormField = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 25px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 25px;
 `;
 
 const Input = styled.input`
-    padding: 10px;
-    border: 1px solid #ddd;
-    border-radius: 15px;
-    font-size: 1.5rem;
-   background-color: black;
-    color: white;
-    transition: border-color 0.3s;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 15px;
+  font-size: 1.5rem;
+  background-color: black;
+  color: white;
+  transition: border-color 0.3s;
 
-    &:focus {
-        border-color: #007bff;
-        outline: none;
-    }
+  &:focus {
+    border-color: #007bff;
+    outline: none;
+  }
 `;
 const Textarea = styled.textarea`
-    padding: 10px;
-    border: 1px solid #ddd;
-    border-radius: 15px;
-    font-size: 1.5rem;
-    background-color: black;
-    color: white;
-    transition: border-color 0.3s;
-    height: 150px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 15px;
+  font-size: 1.5rem;
+  background-color: black;
+  color: white;
+  transition: border-color 0.3s;
+  height: 150px;
 
-    &:focus {
-        border-color: #007bff;
-        outline: none;
-    }
+  &:focus {
+    border-color: #007bff;
+    outline: none;
+  }
 `;
 const ButtonContainer = styled.div`
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 `;
 
 const SubmitButton = styled.button`
-    padding: 10px 20px;
-    background-color: #e50914;
-    color: #fff;
-    border: none;
-    border-radius: 15px;
-    cursor: pointer;
-    font-size: 1.5rem;
-    width: 100%;
-    max-width: 200px; 
+  padding: 10px 20px;
+  background-color: #e50914;
+  color: #fff;
+  border: none;
+  border-radius: 15px;
+  cursor: pointer;
+  font-size: 1.5rem;
+  width: 100%;
+  max-width: 200px;
 
-    &:hover {
-        background-color: #b20710;
-    }
+  &:hover {
+    background-color: #b20710;
+  }
 `;
 
 const SentMessage = styled.p`
-    margin-top: 20px;
-    font-size: 2rem;
-    color: green;
-    text-align: center;
+  margin-top: 20px;
+  font-size: 2rem;
+  color: green;
+  text-align: center;
 `;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,128 +1,127 @@
 
+
 import React from "react";
 import styled from "styled-components";
 import { FaGithub } from "react-icons/fa";
 
 const Projects = () => {
-  const projectsInfo = [
+  const projects = [
+    {
+      title: "FinPort",
+      imgSrc: "/assets/finport.JPG",
+      repoLink: "https://github.com/ClauellaN/finalProject-FinPort.git",
+      liveDemo: "https://finport.vercel.app/",
+      description:
+        "A financing portal to help merchants manage clients and payments seamlessly.",
+    },
+    {
+      title: "Wearables Store",
+      imgSrc: "/assets/eComm.JPG",
+      repoLink: "https://github.com/ClauellaN/finalProject-FinPort.git",
+      liveDemo: "https://finport.vercel.app/",
+      description:
+        "An e-commerce store offering a seamless shopping experience with easy checkouts.",
+    },
     {
       title: "Nyan Cat",
       imgSrc: "/assets/nyan-cat.jpg",
       repoLink: "https://github.com/ClauellaN/project-js-nyan-cat.git",
       liveDemo: "https://nyan-cat.vercel.app",
-      description: "Experience a thrilling game with Nyan Cat the astronaut. Help him navigate through falling arrows to safely reach the moon. You have 3 lives to complete your mission.",
-      instructions: "View the source code on GitHub.",
+      description:
+        "Experience a thrilling game with Nyan Cat the astronaut. Help him navigate through falling arrows to safely reach the moon.",
     },
     {
       title: "Sling Air",
       imgSrc: "/assets/slingAir1.jpg",
       repoLink: "https://github.com/ClauellaN/project-slingair.git",
       liveDemo: "https://sling-air.vercel.app",
-      description: "A user-friendly flight booking application. Easily book flights and manage your reservations.",
-      instructions: "View the source code on GitHub.",
+      description:
+        "A user-friendly flight booking application. Easily book flights and manage your reservations.",
     },
     {
       title: "Tic-Tac-Toe",
       imgSrc: "/assets/TicTacToe.JPG",
       repoLink: "https://github.com/ClauellaN/react-useState.git",
       liveDemo: "https://tic-tac-toe.vercel.app",
-      description: "A classic Tic-Tac-Toe game built with React. Play against the computer or challenge a friend.",
-      instructions: "View the source code on GitHub.",
+      description: "A classic Tic-Tac-Toe game built with React.",
     },
   ];
 
   return (
-    <ProjectsWrapper>
-      <Title>PROJECTS</Title>
-      <ProjectGrid>
-        {projectsInfo.map((project) => (
-          <ProjectItem key={project.title}>
-            <img src={project.imgSrc} />
+    <Wrapper>
+      <Title>Projects</Title>
+      <Grid>
+        {projects.map((project) => (
+          <Card key={project.title}>
+            <Image src={project.imgSrc} alt={project.title} />
             <Overlay>
               <ProjectTitle>{project.title}</ProjectTitle>
               <Description>{project.description}</Description>
-              <ProjectLinks>
-                {project.liveDemo && (
-                  <Button href={project.liveDemo} target="_blank">
-                    Live Demo
-                  </Button>
-                )}
-              </ProjectLinks>
-              <Instructions>{project.instructions}</Instructions>
-              <ProjectLinks>
+              <ButtonGroup>
+                <Button href={project.liveDemo} target="_blank">
+                  Live Demo
+                </Button>
                 <IconButton href={project.repoLink} target="_blank">
-                  <FaGithub size={20} />
+                  <FaGithub size={24} />
                 </IconButton>
-              </ProjectLinks>
+              </ButtonGroup>
             </Overlay>
-          </ProjectItem>
+          </Card>
         ))}
-      </ProjectGrid>
-    </ProjectsWrapper>
+      </Grid>
+    </Wrapper>
   );
 };
 
 export default Projects;
 
-// Styled components
-const ProjectsWrapper = styled.div`
+const Wrapper = styled.div`
+  padding: 60px;
+  background-color: #1a1a1a;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  padding: 45px;
-  min-height: 100vh;
-  background-color: #1a1a1a;
 `;
 
-const Title = styled.h2`
-  font-size: 2rem;
-  margin-bottom: 100px;
-  color: white;
+const Title = styled.h1`
+  font-size: 2.5rem;
+  color: #fff;
+  margin-bottom: 40px;
 
   @media (max-width: 768px) {
-    font-size: 1.5rem;
+    font-size: 2rem;
   }
 `;
 
-const ProjectGrid = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 20px;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 30px;
   width: 100%;
   max-width: 1200px;
+`;
 
-  @media (max-width: 1000px) {
-    gap: 10px;
+const Card = styled.div`
+  position: relative;
+  border-radius: 15px;
+  overflow: hidden;
+  transition: transform 0.3s ease;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+
+  &:hover {
+    transform: translateY(-5px);
   }
 `;
 
-const ProjectItem = styled.div`
-  position: relative;
-  overflow: hidden;
+const Image = styled.img`
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
   transition: transform 0.3s ease;
-  height: 500px;
-  width: 500px;
-  border-radius: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: #333;
 
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    transition: transform 0.3s ease;
-  }
-
-  &:hover img {
-    transform: scale(1.05);
-  }
-
-  &:hover div {
-    opacity: 1;
+  ${Card}:hover & {
+    transform: scale(1.1);
   }
 `;
 
@@ -130,22 +129,25 @@ const Overlay = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  height: 100%;
   width: 100%;
-  background: black;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.7);
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   opacity: 0;
   transition: opacity 0.3s ease;
-  padding: 20px; 
+
+  ${Card}:hover & {
+    opacity: 9;
+  }
 `;
 
-const ProjectTitle = styled.h3`
-  margin: 15px 0;
+const ProjectTitle = styled.h2`
   font-size: 1.8rem;
   color: #fff;
+  margin-bottom: 10px;
 
   @media (max-width: 768px) {
     font-size: 1.5rem;
@@ -153,41 +155,23 @@ const ProjectTitle = styled.h3`
 `;
 
 const Description = styled.p`
-  margin: 10px 0;
-  font-size: 1.2rem;
+  font-size: 1rem;
   color: #ddd;
   text-align: center;
-
-  @media (max-width: 768px) {
-    font-size: 1rem;
-  }
+  margin-bottom: 15px;
 `;
 
-const Instructions = styled.p`
-  margin: 10px 0;
-  font-size: 1rem;
-  color: #aaa;
-  text-align: center;
-
-  @media (max-width: 768px) {
-    font-size: 0.9rem;
-  }
-`;
-
-const ProjectLinks = styled.div`
-  margin-top: 20px;
+const ButtonGroup = styled.div`
   display: flex;
-  justify-content: center;
-  gap: 10px;
-  align-items: center;
+  gap: 15px;
 `;
 
 const Button = styled.a`
-  text-decoration: none;
-  color: white;
   background: linear-gradient(to right, #0070f3, #005bb5);
   padding: 10px 20px;
-  border-radius: 5px;
+  color: white;
+  border-radius: 8px;
+  text-decoration: none;
   font-weight: bold;
   transition: background 0.3s ease, transform 0.3s ease;
 
@@ -195,42 +179,22 @@ const Button = styled.a`
     background: linear-gradient(to right, #005bb5, #0070f3);
     transform: translateY(-2px);
   }
-
-  @media (max-width: 768px) {
-    padding: 8px 16px;
-    font-size: 0.9rem;
-  }
 `;
 
 const IconButton = styled.a`
   display: flex;
   align-items: center;
   justify-content: center;
+  background-color: #444;
   width: 40px;
   height: 40px;
+  border-radius: 50%;
   text-decoration: none;
   color: white;
-  background-color: #333;
-  border-radius: 50%;
   transition: background-color 0.3s ease, transform 0.3s ease;
 
   &:hover {
-    background-color: #555;
+    background-color: #666;
     transform: translateY(-2px);
-  }
-
-  svg {
-    width: 20px;
-    height: 20px;
-  }
-
-  @media (max-width: 768px) {
-    width: 35px;
-    height: 35px;
-
-    svg {
-      width: 18px;
-      height: 18px;
-    }
   }
 `;
